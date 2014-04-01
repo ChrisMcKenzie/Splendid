8866903babba2bc41964cff14a81c70658b893e5
ChrisMcKenzie <chris@kbsurfer.com>
2014-04-01 13:24:57 -0700

    Initial Native commit

---
+++ app/scripts/modules/header.js
@@ -0,0 +1,68 @@
+'use strict';
+var gui = require('nw.gui');
+var win = gui.Window.get();
+/**
+ * @fileoverview API and Directive for App Header Element.
+ * @author chris@kbsurfer.com (Chris McKenzie)
+ */
+
+angular.module('splendid.header', ['splendid.config', 'splendid.filesystem']).factory('Header',function(){
+	return {
+		message: '',
+        /**
+         * Sets the message on the header notif area.
+         * @param {string} the message to display.
+         */
+		setMessage: function(message){
+			this.message = message;
+		},
+        /**
+         * Close App window.
+         */
+        closeWindow: function(){
+            win.close();
+        },
+        /**
+         * Minimize App window.
+         */
+        minimizeWindow: function(){
+            win.minimize();
+        },
+        /**
+         * Maximize App window.
+         */
+        maximizeWindow: function(){
+            if (this.isMaximized()) {
+                win.unmaximize();
+            } else {
+                win.maximize();
+            }
+        },
+        toggleDevTools: function(){
+            if(win.isDevToolsOpen()) {
+                win.closeDevTools();
+            } else {
+                win.showDevTools();
+            }
+        },
+        /**
+         * Is current Window Maximized.
+         * @return {boolean} true if window is at maximum size.
+         */
+        isMaximized: function(){
+            return window.outerHeight === window.screen.availHeight &&
+                            window.outerWidth === window.screen.availWidth;
+        }
+	};
+}).directive('sHeader', ['BASE_TEMPLATE_PATH', 'Header', function(BASE_TEMPLATE_PATH){
+    return {
+        restrict: 'E', //Element only
+        scope: {}, //Private Scope.
+        templateUrl: BASE_TEMPLATE_PATH + 'header.html', // templates/header.html
+        controller: function($scope, Header, File){
+            //Place Header API on directive scope.
+            $scope.api = Header;
+            $scope.files = File.get();
+        }
+    };
+}]);
---
+++ app/scripts/modules/system.js
@@ -0,0 +1,73 @@
+'use strict';
+ /*jshint unused:false */
+angular.module('splendid.system', ['splendid.filesystem']).factory('UI', function($rootScope, $q, $timeout, File, Editor){
+	var _status = {};
+
+	return {
+        init: function(){
+
+        },
+        chooseFile: function(name) {
+            var chooser = document.querySelector(name);
+            chooser.addEventListener("change", function(evt) {
+
+            }, false);
+
+            chooser.click();
+        },
+        openFile: function(){
+            File.open().then(function(file){
+                //$rootScope.currentFile = file;
+                File.setCurrentFile(file);
+            });
+        },
+        switchFile: function(name){
+            File.setCurrentFile(File.get(name));
+        },
+        saveFile: function(file){
+            console.log(file);
+            File.save(file).then(function(){
+                //console.log(file.name + ' saved!');
+                this.setStatus('File saved...');
+            });
+        },
+        closeFile: function(file){
+            var f = File.get(file.name);
+
+
+            if(f.name === file.name) {
+                File.setCurrentFile(file); //TODO: make open previous file
+            }
+
+            File.close(file);
+        },
+		getStatus: function(){
+			return _status;
+		},
+		setStatus: function(msg){
+			_status.message = msg;
+			$timeout(function(){
+				_status.message = '';
+			}, 3000);
+		},
+		hideMenu: function(){
+			$.UIkit.offcanvas.offcanvas.hide();
+		},
+		showMenu: function(){
+			$.UIkit.offcanvas.offcanvas.show();
+		},
+		registerShortcut: function(opts, callback){
+			/*for(var keys in opts.keys){
+				var key = opts.keys[keys].replace('+', '-');
+				_editor.commands.addCommand({
+                    name: opts.name,
+                    bindKey: key,
+                    exec: callback,
+                    readOnly: true // false if this command should not apply in readOnly mode
+				});
+			}*/
+
+			Mousetrap.bind(opts.keys, callback);
+		}
+	};
+});
